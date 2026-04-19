8fe3cc97806a17944142cbb36ef0f432ecf1d29c
Marie Bethell <bethe009@csusm.edu>
2026-04-18 17:08:10 -0700

    Add timeout code

--- scraper.py
+++ scraper.py
@@ -1,19 +1,24 @@
 from playwright.sync_api import sync_playwright
+import time
 
 def get_wunderground_forecast(zipcode: str):
     """
     Get 10-day forecast from WunderGround.
     """
+    start = time.time()
 
     url = f"https://www.wunderground.com/forecast/us/ca/{zipcode}"
     print(url)
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(args=["--no-sandbox"])
+        browser = p.chromium.launch(
+            args=["--no-sandbox", "--disable-dev-shm-usage"]
+        )
         page = browser.new_page()
 
-        page.goto(url)
-        page.wait_for_selector(".temp-hi")
+        page.goto(url, wait_until="domcontentloaded", timeout=15000)
+
+        page.wait_for_selector(".temp-hi", timeout=10000)
 
         highs = page.locator(".temp-hi").all_text_contents()
         lows = page.locator(".temp-lo").all_text_contents()
@@ -23,3 +28,5 @@ def get_wunderground_forecast(zipcode: str):
             print(d, h, l)
 
         browser.close()
+    
+    print("Time taken:", time.time() - start)
